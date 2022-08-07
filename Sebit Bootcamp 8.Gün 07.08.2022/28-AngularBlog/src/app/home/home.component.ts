6e9cf99f13eef88a469e2c6a762d29c17687aba3
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-08-07 19:58:43 +0300

    Add files via upload

---
+++ Sebit Bootcamp 8.Gün 07.08.2022/28-AngularBlog/src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
+import { MatDialog } from '@angular/material/dialog';
+import { BlogDialogComponent } from './blog-dialog/blog-dialog.component';
+
+// interface Blog {
+//   title:string,
+//   body:string,
+//   imageId:number,
+//   userId:number,
+//   id:number
+// }
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  pageSize = 8;
+  page = 13;
+  blogData: Array<any> = [];
+  constructor(private blogService: BlogService, public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.getBlogList();
+  }
+  openDialog(element: any, viewOrUpdate: any) {
+    const dialogRef = this.dialog.open(BlogDialogComponent, {
+      width: '450px',
+      data: { blog: element, isUpdate: viewOrUpdate },
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.getBlogList();
+    });
+  }
+  getBlogList() {
+    this.blogService.getPosts().subscribe((res) => {
+      this.blogData = res;
+    });
+  }
+}
