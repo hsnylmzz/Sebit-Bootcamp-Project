4c2e45b545f7d110966611a786dbe3d55773a487
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-08-14 20:18:01 +0300

    Add files via upload

---
+++ Sebit Bootcamp 10.Gün 14.08.2022/30-AngularArticle/Article/src/app/view-article/view-article.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../services/article.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-view-article',
+  templateUrl: './view-article.component.html',
+  styleUrls: ['./view-article.component.css']
+})
+export class ViewArticleComponent implements OnInit {
+  articleId : any;
+  articlesData : Array<any> = [];
+  username : string = '';
+  comment : string = '';
+  constructor(
+    private route : ActivatedRoute,
+    private articleService : ArticleService,
+    public _snackBar : MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.articleId = this.route.snapshot.paramMap.get('articleIndex');
+    this.articleService.getArticles().subscribe((res) => {
+      this.articlesData = res;
+    })
+  }
+  addComment(){
+    if(this.username =='' || this.comment == '')
+    {
+       this. _snackBar.open("Lütfen Tüm Alanlarınızı Doldurunuz","Kapat");
+    }
+    else{
+      let obj = {
+        username: this.username,
+        comment : this.comment,
+      }
+      this.articlesData[this.articleId].comments.push(obj);
+      this.articleService.updateArticle(this.articlesData[this.articleId])
+      .subscribe((res) => {
+          //inputları sıfırlamak için
+          this.username = '';
+          this.comment = '';
+      });
+    }
+  }
+}
